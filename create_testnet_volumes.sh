9c5a520e9b09861a32491e1dbb04f8561c4005f6
J. Ayo Akinyele <ayo@boltlabs.io>
2019-01-30 08:07:38 -0800

    add testnet script

---
+++ create_testnet_volumes.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+docker volume create testnet-chain
+docker volume create testnet-params
+
+docker run -itd \
+--name zcashd-testnet \
+--mount source=testnet-chain,destination=/home/zcash/.zcash \
+--mount source=testnet-params,destination=/home/zcash/.zcash-params \
+-v $CUR_PATH/testnet.daemon.conf:/home/zcash/.zcash/zcash.conf \
+boltlabs/zcashd-lite:v2.0.2
+
+docker exec -it zcashd-testnet bash -c "fetch-params.sh"
+
+# Verify that fetch-params a success
+docker exec -it zcashd-testnet bash -c "ls /home/zcash/.zcash-params"
+
+docker exec -itd zcashd-testnet bash -c "zcashd"
+
