be5e54e183aba74990460d6466a89152793b9a72
J. Ayo Akinyele <ayo@boltlabs.io>
2019-05-19 17:49:53 -0400

    Updates to container build

--- build_image.sh
+++ build_image.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-export TAG=v2.0.4
+export TAG=v2.0.5
 docker build --build-arg TAG -t boltlabs/zcashd-lite:$TAG --memory-swap -1 .
--- explorer/build_image.sh
+++ explorer/build_image.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 #export TAG=v1.1.1
-export TAG=v2.0.2
+export TAG=v2.0.5
 docker build --build-arg TAG -t boltlabs/zcashd-explore:$TAG --memory-swap -1 .
